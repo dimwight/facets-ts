35ea5d981d0a4e04c4e1a7e256b70b9afdef1c38
David Wright <dimwight@ntlworld.com>
2017-10-13 14:20:19 +0100

    NotifyingCore

--- facets/core/Notifying.js
+++ facets/core/Notifying.js
@@ -1,3 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var NotifyingCore = /** @class */ (function () {
+    function NotifyingCore() {
+    }
+    NotifyingCore.prototype.setNotifiable = function (n) {
+        throw new Error("Method not implemented.");
+    };
+    NotifyingCore.prototype.notifiable = function () {
+        throw new Error("Method not implemented.");
+    };
+    NotifyingCore.prototype.elements = function () {
+        throw new Error("Method not implemented.");
+    };
+    NotifyingCore.prototype.notifyParent = function () {
+        throw new Error("Method not implemented.");
+    };
+    NotifyingCore.prototype.notify = function (notice) {
+        throw new Error("Method not implemented.");
+    };
+    return NotifyingCore;
+}());
 //# sourceMappingURL=Notifying.js.map
--- facets/core/Notifying.ts
+++ facets/core/Notifying.ts
@@ -7,4 +7,21 @@ interface Notifying extends Notifiable{
   elements():Notifying[]
   notifyParent()
 } 
+class NotifyingCore implements Notifying{
+  setNotifiable(n: Notifiable) {
+    throw new Error("Method not implemented.");
+  }
+  notifiable(): Notifiable {
+    throw new Error("Method not implemented.");
+  }
+  elements(): Notifying[] {
+    throw new Error("Method not implemented.");
+  }
+  notifyParent() {
+    throw new Error("Method not implemented.");
+  }
+  notify(notice: any) {
+    throw new Error("Method not implemented.");
+  }
+}
   
