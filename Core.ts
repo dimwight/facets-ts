ab3061478eec2c31117ea4aa02b72fbe6faca853
David Wright <dimwight@ntlworld.com>
2017-10-13 12:02:24 +0100

    retarget

--- Core.ts
+++ Core.ts
@@ -3,32 +3,47 @@ export function trace(msg, thing){
 }
 export interface Target{
   title:string
+  elements:()=>Target[]
+  }
+export interface Notifiable{
+  notify(notice)
 }
-export interface Targeter{
+export interface Targeter extends Notifiable{
   title:()=>string
   target:()=>Target
   setNotifiable(n:Notifiable)
   retarget(t:Target)
   elements:()=>Targeter[]
 }
-export interface Notifiable{
-  notify(notice)
-}
+
 export class TargetCore implements Target{
   constructor(readonly title:string,readonly members?:Target[]){}
   newTargeter():Targeter{
     return new TargeterCore()
+  }
+  elements=():Target[]=>{
+    return []
   }
 } 
 export class TargeterCore implements Targeter{
   private elements_: Targeter[];
   private title_='Untargeted'
   private target_:Target
+  notify(notice){
+    throw new Error('Not implemented in TargeterCore')
+  }
   setNotifiable(Notifiable){
     
   }
   retarget(target:Target){
+    if(!target)throw new Error('Missing target')
     this.target_=target
+    const targets:Target[]=target.elements();
+    if(!this.elements_)this.elements_=targets.map<Targeter>((target)=>{
+        let element=(<TargetCore>target).newTargeter();
+        element.setNotifiable(this);
+        return element;
+      })
   }
   title=()=>this.target?this.target_.title:this.title_ 
   target=():Target=>{
