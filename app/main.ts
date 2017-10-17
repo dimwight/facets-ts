28b289c0a32e667155c9ca340300bcd39251c03a
David Wright <dimwight@ntlworld.com>
2017-10-17 11:12:45 +0100

    Gisty

--- app/main.ts
+++ app/main.ts
@@ -28,7 +28,7 @@ function buildLayout(){
   core.attachFacet(TITLE_FIRST,update=>trace('Facet updating with '+update));
 }
 function main(){
-  swapArrayElement([0,1,2,3,4],3,false);
+  swapArrayElement([0,1,2],'1',false);
   if(true)return;
   trace('Building surface');
   core.buildTargeterTree(newTargetTree());
--- facets/bits/Bits.ts
+++ facets/bits/Bits.ts
@@ -1,28 +1,74 @@
-export function traceThing(msg:string, thing) {
-  if(msg.charAt(0)!='^')
-    console.info((true?'':'Facets') + msg, JSON.stringify(thing, true ? null : (key, value) => {
-      console.log(key)
-      return value
-    }, 1))
+/**
+ * Simplifies instrumenting code
+ * @param top heading for message 
+ * @param thing suitable for JSON serialization
+ */
+export function traceThing(top:string, thing) {
+
+  //Allow for quick disable
+  if(top.charAt(0)=='^')return;
+
+  //Allow for callback eg to find and kill circular references
+  const callback=true ? null : (key, value) => {
+    console.log(key);
+    return value
+  }
+  
+  //Construct body
+  const tail=JSON.stringify(thing, callback, 1);
+  
+  //Issue complete message
+  console.log(top, tail)
 }
 /** Swaps an array element with one of its neighbours. 
- * @param a the array
- * @param index of the element to be swap
- * @param down the direction of the swap
+ * @param source the array to modify
+ * @param index of the element to be swapped
+ * @param down sets the direction of the swap
  */
-export function swapArrayElement(a: any[], index: number, down: boolean) {
+export function swapArrayElement(source: any[], index, down) {
+  /* 
+  1. Define source indices of elements to be swapped.
+  2. Define output indices to swap them to.
+  3. Split the source array without the swap indices.
+  4. Insert the swapped elements.
+  */
+
+  // Debug?
   traceThing('^swapArrayElement', { index: index, down: down })
-  const lowerFrom = down?index:index+1, upperFrom = down?index-1:index,
-   lowerTo = down ? index-1 : index,upperTo = down ? index : index+1;
+
+  // Guard against string!
+  const indexNum=Number(index);
+
+  //Define source and target indices
+  const lowerFrom = down?indexNum:indexNum+1,
+    upperFrom = down?indexNum-1:indexNum;
+  const lowerTo = down ? indexNum-1 : indexNum,
+    upperTo = down ? indexNum : indexNum+1;
+
+  // Check for out of bounds
   const names=['index','lowerFrom','upperFrom','lowerTo','upperTo'];
-  [index,lowerFrom,upperFrom,lowerTo,upperTo].forEach((n,at)=>{
-    if(n<0||n>=a.length)throw new Error(`Index out of range: ${names[at]}=${n}`)
+  [indexNum,lowerFrom,upperFrom,lowerTo,upperTo].forEach((n,at)=>{
+
+    //Index out of bounds?
+    if(n<0||n>=source.length)throw new Error(`Index out of range: ${names[at]}=${n}`)
   })
+
+  // Debug?
   traceThing('^swapArrayElement', { lowerFrom: lowerFrom, upperFrom: upperFrom,
      lowerTo: lowerTo, upperTo: upperTo })
-    const top = a.slice(0, lowerTo), tail = a.slice(upperTo+1),
-    shifted = top.concat(a[lowerFrom],a[upperFrom],tail);
+
+  // Define unaffected regions
+  const top = source.slice(0, lowerTo), tail = source.slice(upperTo+1);
+
+  //Assemble output
+  const shifted = top.concat(source[lowerFrom],source[upperFrom],tail);
+
+  // Debug?
   traceThing('^swapArrayElement~', false?{top:top,tail:tail}:shifted)
-  a.splice(0,a.length,shifted);
-  traceThing('swapArrayElement~~', a)
+
+  //Rebuild source
+  source.splice(0,source.length,shifted);
+
+  //Final check?
+  traceThing('swapArrayElement~~', source)
 }
