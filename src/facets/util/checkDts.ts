8368b16f6c02b067fabc91c50204259d895abcfd
David Wright <dimwight@ntlworld.com>
2017-11-01 14:42:29 +0000

    No comments

--- src/facets/util/checkDts.ts
+++ src/facets/util/checkDts.ts
@@ -1,8 +1,21 @@
 import * as fs from 'fs-extra';
-import {traceThing} from './export';
+import { traceThing } from './export';
 
-let content:string = fs.readFileSync('index.d.ts', 'utf8').replace(/\r\n/,'\n');
-// console.log(content.substr(0,200)+'\n-------------');
-content.match(/(\/\*\*[^/]+\/\s*)\w[^;{/]+/g).forEach((match,at)=>{
-  if(at<200)console.log(match+'\n---------');
-});
+function main() {
+  let src = 'index.d.ts',dest=true?'index_.d.ts':src, facets='Facets.d.txt';
+  let content = fs.readFileSync(src, 'utf8').replace(/\r\n/, '\n'),
+    mods=fs.readFileSync(facets, 'utf8').replace('Facets.','');
+  // console.log(content.substr(0,200)+'\n-------------');
+  function handleChunk(chunk:string, at) {
+    chunk=chunk.replace(/\/\*\*[^/]+\/\s*/,'');
+    if(false)console.log(chunk + '\n---------');
+    if(mods.includes(chunk)){
+      console.log('Found match='+chunk.substr(0,50));
+      return;
+    }
+    content=content.replace(chunk,chunk+'\n');
+  }
+  content.match(/\/\*\*[^/]+\/\s*\w[^\n]+/g).forEach(handleChunk);
+  fs.writeFileSync(dest,content);
+}
+main();
