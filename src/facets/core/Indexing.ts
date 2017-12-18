16f397f809177aab8fb98b4028254f527fe466bb
David Wright <dimwight@ntlworld.com>
2017-12-18 08:32:54 +0000

    Indexing

--- src/facets/core/Indexing.ts
+++ src/facets/core/Indexing.ts
@@ -4,48 +4,43 @@ import {
 import { 
   IndexingCoupler 
 } from '../index';
+import { SimpleState } from '../Facets';
 export class Indexing extends TargetCore {  
-  coupler: IndexingCoupler;  
-  private index_: number;  
   private indexings: any[];  
-  constructor (title: string, coupler: IndexingCoupler){
-      super(title);
-      this.coupler = this.coupler;
+  constructor (title: string, private coupler: IndexingCoupler){
+    super(title);
   }
   index(): number {
-      return this.index_;
+    return this.state_ as number;
   }
   setIndex(index: number) {
-      let first = !this.index_;
-      this.index_ = index;
-      if (!first)this.coupler.targetStateUpdated(this.state(),this.title());
+    let first = this.state_===TargetCore.NoState;
+    this.state_ = index;
+    if (!first)this.coupler.targetStateUpdated(this.state_,this.title());
   }
   indexables(): any[] {
-      let indexables: any[] = this.coupler.getIndexables(this.title());
-      if (!indexables|| (indexables.length === 0)) 
-          throw new Error('Null or empty indexables in ' + this);
-      else return indexables;
+    let indexables: any[] = this.coupler.getIndexables(this.title());
+    if (!indexables||indexables.length === 0) 
+        throw new Error('Missing or empty indexables in ' + this);
+    else return indexables;
   }
   uiSelectables(): string[] {
-    let newSelectable=this.coupler.newUiSelectable
-        ||((indexable)=>(indexable as string));
-    return this.indexables().map(each=>newSelectable(each));      
+    let getSelectable=this.coupler.newUiSelectable||((i)=>(i as string));
+    return this.indexables().map(i=>getSelectable(i));      
   }
   indexed(): any {
-      if (!this.index_)throw new Error(('No index in ' + this.title()));
-      else return this.indexables()[this.index_];
+    if (this.state_===TargetCore.NoState)
+      throw new Error(('No index in ' + this.title()));
+    else return this.indexables()[this.state_ as number];
   }
   setIndexed(indexable: any) {
-      for(const [at,i] of this.indexables().entries())
-          if (i === indexable) {
-              this.setIndex(i as number);
-              break;
-          }          
+    for(const [at,i] of this.indexables().entries())
+        if (i === indexable) {
+            this.setIndex(i as number);
+            break;
+        }          
   }
-  state(): any {
-      return this.index_;
-  }
-  updateState(update: any) {
-      this.setIndex(update);
+  updateState(update: SimpleState) {
+    this.setIndex(update as number);
   }
 }
--- src/facets/core/TargetCore.ts
+++ src/facets/core/TargetCore.ts
@@ -10,11 +10,12 @@ import { SimpleState } from '../../facets/Facets';
 import { Notifiable } from './Notifiable';
 export class TargetCore extends NotifyingCore implements Targety {
   private readonly type='TargetCore';
-  private state_:SimpleState='No state set';
+  protected static NoState='No state set';
+  protected state_:SimpleState=TargetCore.NoState;
   state(): SimpleState {
     return this.state_;
   }
-  constructor(private readonly title_:string,private readonly elements_?:Targety[]){
+  constructor(protected readonly title_:string,private readonly elements_?:Targety[]){
     super();
   }
   notifiesTargeter():boolean{
